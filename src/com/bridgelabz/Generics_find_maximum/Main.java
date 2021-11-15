dbbb07a6d52ead782f4ca50e9d870c45b5c7772f
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-15 23:05:35 +0530

    ADDED: starting the program of finding the maximum using Generics

---
+++ src/com/bridgelabz/Generics_find_maximum/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.Generics_find_maximum;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO FIND MAXIMUM USING GENERICS");
+    }
+}
