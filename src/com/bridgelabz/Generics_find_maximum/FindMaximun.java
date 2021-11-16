6a5bf939886c01d8685a5c9dd4d63d90d85940ec
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-16 12:52:34 +0530

    ADDED: Generic method to find maximum

--- src/com/bridgelabz/Generics_find_maximum/FindMaximun.java
+++ src/com/bridgelabz/Generics_find_maximum/FindMaximun.java
@@ -1,28 +1,8 @@
 package com.bridgelabz.Generics_find_maximum;
 
 public class FindMaximun {
-    public Integer getMax(Integer firstValue, Integer secondValue, Integer thirdValue) {
-        Integer max = firstValue;
-        if (secondValue.compareTo(max) > 0)
-            max = secondValue;
-        if (thirdValue.compareTo(max) > 0)
-            max = thirdValue;
-
-        return max;
-    }
-
-    public Float getMax(Float firstValue, Float secondValue, Float thirdValue) {
-        Float max = firstValue;
-        if (secondValue.compareTo(max) > 0)
-            max = secondValue;
-        if (thirdValue.compareTo(max) > 0)
-            max = thirdValue;
-
-        return max;
-    }
-
-    public String getMax(String firstValue, String secondValue, String thirdValue) {
-        String max = firstValue;
+    public <E extends Comparable<E>> E getMax(E firstValue, E secondValue, E thirdValue) {
+        E max = firstValue;
         if (secondValue.compareTo(max) > 0)
             max = secondValue;
         if (thirdValue.compareTo(max) > 0)
@@ -34,7 +14,11 @@ public class FindMaximun {
     public static void main(String[] args) {
 
         FindMaximum maximum = new FindMaximum();
-        System.out.println("Maximum Value:");
+        System.out.println("Maximum Value Of Integer:");
+        System.out.println(maximum.getMax(50, 20, 70));
+        System.out.println("Maximum Value Of Flot:");
+        System.out.println(maximum.getMax(15.3f, 45.5f, 13.2f));
+        System.out.println("Maximum Value Of String:");
         System.out.println(maximum.getMax("Apple", "Peach", "Banana"));
 
     }
